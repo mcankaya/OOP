ceb6873dda445d081b397e2e832572cab49728d9
Mehmet <mhmcankaya@gmail.com>
2017-05-02 14:52:34 +0300

    add date and c++ code

---
+++ rmse.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+// RMSE function
+float stat_rmse(float *x, float *y, int N) {
+  float total = 0.0;
+  for (int i = 0; i < N; i++) {
+    total = total+pow(x[i]-y[i], 2);
+  }
+  return sqrt(total/(float)N);
+}
+
+// main function
+int main() {
+  // get size
+  int N = 0;
+  cin >> N;
+  cout << "Problem Size = " << N << endl;
+
+  // read data
+  float x[N];
+  for (int i = 0; i < N; i++) {
+    cin >> x[i];
+    cout << "x[" << i << "] = " << x[i] << endl;
+  }
+
+  float y[N];
+  for (int i = 0; i < N; i++) {
+    cin >> y[i];
+    cout << "y[" << i << "] = " << y[i] << endl;
+  }
+
+  // calculate RMSE
+  float rmse = stat_rmse(x, y, N);
+  cout << "Calculated RMSE value = " << rmse << endl;
+
+  return 0;
+}
